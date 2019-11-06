30a54a48589dfd029cea11b68aa141efa7e5822f
Azamat Zyntemirov <zyntemirov@gmail.com>
2019-11-06 14:01:30 +0600

    -stadium news, update links fixed

--- pages/dashboardAdmin/stadiums/index.vue
+++ pages/dashboardAdmin/stadiums/index.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="content">
     <div class="card-body">
-      <nuxt-link to="/dashboard/stadiums/new" class="btn btn-primary btn-block btn-stadium" style="width: 200px; margin-left: 10px"><i class="fa fa-plus" style="padding-right:3px; font-size: 25px; text-align: center; vertical-align: middle "></i>  Добавить стадион</nuxt-link>
+      <nuxt-link to="/dashboardAdmin/stadiums/new" class="btn btn-primary btn-block btn-stadium" style="width: 200px; margin-left: 10px"><i class="fa fa-plus" style="padding-right:3px; font-size: 25px; text-align: center; vertical-align: middle "></i>  Добавить стадион</nuxt-link>
       <div class="row" style="margin-top: 25px">
         <div v-for="(stadium, key) in stadiums" :key="key" class="col-sm-3 text-center blocks">
           <img :src="'https://firebasestorage.googleapis.com/v0/b/stadion-e9852.appspot.com/o/stadium%2F'+ stadium.stadiumId + '%2Fthumb_small_' + stadium.imgNames[0] + '?alt=media'" @error="imageLoadError({stadiumId: stadium.stadiumId, name:stadium.imgNames[0]})" class="box-shadow b-r-15" style="max-width: 100%">
           <p v-if="stadium.name.length > 25">{{ stadium.name.slice(0,25)}} ...</p>
           <p v-else>{{ stadium.name }}</p>
-          <nuxt-link class="btn btn-success b-r-9" :to="'/dashboard/stadiums/edit/'+key"><i class="fa fa-pencil"></i>
+          <nuxt-link class="btn btn-success b-r-9" :to="'/dashboardAdmin/stadiums/edit/'+key"><i class="fa fa-pencil"></i>
           </nuxt-link>
           <a class="btn btn-danger b-r-9" @click.prevent="removeStadium({id: key, name: stadium.name})"><i class="fa fa-trash"></i></a>
         </div>
