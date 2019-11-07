86a752a2343b1e4c859a7dccb72fcd172f1c9ae0
Azamat Zyntemirov <zyntemirov@gmail.com>
2019-11-07 15:38:59 +0600

    -Admin stadium can show histories of own stadiums

---
+++ pages/dashboardAdmin/histories/index.vue
@@ -0,0 +1,93 @@
+<template>
+  <div class="content">
+    <div class="card-body">
+      <div class="row" style="margin-top: 25px">
+        <div v-for="(stadium, key) in stadiums" :key="key" class="col-sm-3 text-center blocks">
+          <nuxt-link :to="'/dashboardAdmin/histories/show/'+key" class="close_icon fa fa-history fa-3x"></nuxt-link>
+          <img :src="'https://firebasestorage.googleapis.com/v0/b/stadion-e9852.appspot.com/o/stadium%2F'+ stadium.stadiumId + '%2Fthumb_small_' + stadium.imgNames[0] + '?alt=media'" @error="imageLoadError({stadiumId: stadium.stadiumId, name:stadium.imgNames[0]})" class="box-shadow b-r-15" style="max-width: 100%">
+          <p v-if="stadium.name.length > 25">{{ stadium.name.slice(0,25)}} ...</p>
+          <p v-else>{{ stadium.name }}</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+    import {mapGetters, mapActions} from 'vuex'
+
+    export default {
+        layout: 'dashboard',
+        name: "stadiums",
+        computed: {
+            ...mapGetters({stadiums: 'stadiums/stadiums'}),
+        },
+
+        methods: {
+            ...mapActions({
+                del: 'stadiums/delete'
+            }),
+            async imageLoadError(data){
+                event.target.src = 'https://firebasestorage.googleapis.com/v0/b/stadion-e9852.appspot.com/o/stadium%2F' + data.stadiumId + '%2F'+ data.name + '?alt=media';
+            },
+            async removeStadium(data) {
+                if (confirm("Вы действительно хотите удалить " + data.name + "?")) {
+                    await this.del(data)
+                        .then(() => {
+                            this.$uikit.notification({
+                                message: 'Успешно удалено!',
+                                status: 'success',
+                                pos: 'top-center',
+                                timeout: 2000
+                            });
+                        })
+                        .catch((error) =>{
+                            console.log('Удалить не удалось: ' + error.message);
+                        });
+                }
+            }
+        }
+    }
+</script>
+
+<style scoped>
+  p {
+    margin: 7px
+  }
+
+  img {
+    width: 250px;
+    height: 186px;
+    object-fit: cover;
+  }
+
+  .blocks {
+    margin-bottom: 30px;
+  }
+
+  .b-r-15 {
+    border-radius: 15px;
+  }
+
+  .b-r-9 {
+    border-radius: 9px;
+  }
+
+  .box-shadow {
+    width: 250px;
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+    text-align: center;
+  }
+
+  .close_icon {
+    background: rgba(0, 0, 0, 0.5);
+    padding: 5px 10px;
+    border-radius: 500px;
+    position: absolute;
+    top: 41%;
+    color: white;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    cursor: pointer;
+  }
+</style>
---
+++ pages/dashboardAdmin/histories/show/_id.vue
@@ -0,0 +1,123 @@
+<template>
+  <div class="content">
+    <div class="card">
+      <div class="card-header text-center">
+        <h4 class="m-0">История заказов {{ stadium }}</h4>
+      </div>
+      <div class="card-body" style="padding: 15px">
+          <div class="table-responsive" v-if="historiesList.length !== 0">
+            <table id="zero_config" class="table table-striped table-bordered">
+              <thead>
+              <tr style="text-align: center">
+                <th>Имя клиента</th>
+                <th>Телефон номер клиента</th>
+                <th>Общий сумма</th>
+                <th>Дата</th>
+                <th>начальное время</th>
+                <th>окончания время</th>
+                <th>Статус</th>
+              </tr>
+              </thead>
+              <tbody>
+              <tr v-for="(history,key) in historiesList" v-bind:key="key" style="text-align: center">
+                <td style="width: 20%"> {{ history.user_name }} </td>
+                <td style="width: 15%"> {{ history.user_phone }}</td>
+                <td style="width: 15%"> {{ history.totalPrice }} сом</td>
+                <td style="width: 15%"> {{ history.date }} </td>
+                <td style="width: 15%;"> {{ history.start_time }}</td>
+                <td style="width: 15%"> {{ history.end_time }}</td>
+                <td v-if="history.status === 'accepted'" style="background-color: chartreuse"> Принято </td>
+                <td v-else-if="history.status === 'rejected'" style="background-color: red"> Отклонено </td>
+                <td v-else style="background-color: #e0a800"> Ожидание </td>
+              </tr>
+              </tbody>
+            </table>
+          </div>
+
+          <div class="table-responsive" v-else-if="check && historiesList.length === 0">
+            <h5 class="m-0">Нет история заказов</h5>
+          </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+    import firebase from 'firebase/app'
+
+    export default {
+        name: "show",
+        data() {
+            return {
+                historiesList: [],
+                stadium: '',
+                check: false
+            }
+        },
+        beforeMount: function () {
+            this.$nextTick(() => {
+                this.readHistoriesByStadiumId();
+                this.readOneStadium();
+            })
+        },
+        methods: {
+            readHistoriesByStadiumId() {
+                let historiesRef = firebase.database().ref('/books').orderByChild("stadium_id").equalTo(this.$route.params.id);
+                historiesRef.once('value', (snapshot) => {
+                    console.log(Object.keys(snapshot.val())[0], snapshot.val()["-LhiYyp2PufrA-o6rGNw"])
+                    for (let i = Object.keys(snapshot.val()).length - 1; i >= 0 ; i--){
+                        this.historiesList.push(snapshot.val()[Object.keys(snapshot.val())[i]])
+                    }
+                });
+
+                setTimeout(() =>{
+                    this.check = true
+                },500);
+            },
+
+            readOneStadium(){
+                let stadiumRef = firebase.database().ref('/stadiums').orderByChild("stadiumId").equalTo(this.$route.params.id);
+                stadiumRef.once('value', (snapshot) => {
+                    this.stadium = snapshot.val()[this.$route.params.id].name;
+                })
+            }
+        },
+    }
+
+</script>
+
+<style scoped>
+  .content form {
+    border-top: none;
+  }
+
+  .image_choose {
+    display: inline-block;
+    position: relative;
+  }
+
+  .pic {
+    width: 8em;
+    height: 8em;
+    object-fit: cover;
+  }
+
+  .close_icon {
+    background: rgba(0, 0, 0, 0.5);
+    padding: 5px 10px;
+    border-radius: 500px;
+    position: absolute;
+    top: 50%;
+    color: white;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    cursor: pointer;
+  }
+
+  .pic {
+    width: 12em;
+    height: 9em;
+    object-fit: cover;
+  }
+
+</style>
